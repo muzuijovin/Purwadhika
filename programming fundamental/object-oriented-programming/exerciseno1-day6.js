63fe8918e7dc18d9f1a636218308404590e6d65f
Jovin Najwan <jovin.najwan@gmail.com>
2026-09-04 17:10:24 +0700

    Revert "day 6: edit exercise"

--- programming fundamental/object-oriented-programming/exerciseno1-day6.js
+++ programming fundamental/object-oriented-programming/exerciseno1-day6.js
@@ -18,79 +18,50 @@ Create a function to calculate array of student data
 ● Average
 */
 
-// class Student {
-//   name = "";
-//   email = "";
-//   age = "";
-//   score = "";
-//   constructor(name, email, age, score) {
-//     this.name = name;
-//     this.email = email;
-//     this.age = age;
-//     this.score = score;
-//   }
+const students = [
+  {
+    name: "Jovin",
+    email: "jovin@gmail.com",
+    age: 23,
+    score: 100,
+  },
+  {
+    name: "Budi",
+    email: "budi@gmail.com",
+    age: 20,
+    score: 80,
+  },
+  {
+    name: "Andi",
+    email: "andi@gmail.com",
+    age: 25,
+    score: 90,
+  },
+];
 
-//   get convertAge() {
-//     return (new Date() - new Date(this?.age))/(1000 * 60 * 60 * 60 * 24)
-//   }
-// }
+function calculateStudentDatas(students) {
+  const scores = students.map((values) => values.score).sort((a, b) => a - b);
+  const ages = students.map((values) => values.age).sort((a, b) => a - b);
 
-// const students = [
-//   new Student("Jovin Najwan", "jovin.najwan@gmail.com", "2003-06-15", 80),
-//   new Student("Budi", "budi@gmail.com", "2002-06-15", 90),
-//   new Student("Andi", "andi@gmail.com", "2001-02-15", 80)
-// ];
+  // scores.sort((a, b) => a - b);
+  // ages.sort((a, b) => a - b);
 
-// calculate(_student) {
-//     const sortedStudentAge = _student.sort((a,b) => a.age - b.age);
-//     const averageStudentAge = _student.reduce((acc, item) => acc + item.age, 0);
-//   }
+  const scoreAverange =
+    scores.reduce((total, score) => total + score, 0) / scores.length;
+  const ageAverange = ages.reduce((total, age) => total + age, 0) / ages.length;
 
+  return {
+    score: {
+      highest: scores[scores.length - 1],
+      lowest: scores[0],
+      averange: scoreAverange,
+    },
+    age: {
+      highest: ages[ages.length - 1],
+      lowest: ages[0],
+      averange: ageAverange,
+    },
+  };
+}
 
-// const students = [
-//   {
-//     name: "Jovin",
-//     email: "jovin@gmail.com",
-//     age: 23,
-//     score: 100,
-//   },
-//   {
-//     name: "Budi",
-//     email: "budi@gmail.com",
-//     age: 20,
-//     score: 80,
-//   },
-//   {
-//     name: "Andi",
-//     email: "andi@gmail.com",
-//     age: 25,
-//     score: 90,
-//   },
-// ];
-
-// function calculateStudentDatas(students) {
-//   const scores = students.map((values) => values.score).sort((a, b) => a - b);
-//   const ages = students.map((values) => values.age).sort((a, b) => a - b);
-
-//   // scores.sort((a, b) => a - b);
-//   // ages.sort((a, b) => a - b);
-
-//   const scoreAverange =
-//     scores.reduce((total, score) => total + score, 0) / scores.length;
-//   const ageAverange = ages.reduce((total, age) => total + age, 0) / ages.length;
-
-//   return {
-//     score: {
-//       highest: scores[scores.length - 1],
-//       lowest: scores[0],
-//       averange: scoreAverange,
-//     },
-//     age: {
-//       highest: ages[ages.length - 1],
-//       lowest: ages[0],
-//       averange: ageAverange,
-//     },
-//   };
-// }
-
-// console.log(calculateStudentDatas(students));
+console.log(calculateStudentDatas(students));
